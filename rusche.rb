e6f3506eb50a34c432da6fc52e91ca5b24c13452
DouglasSherk <github@sherk.me>
2013-10-07 15:08:59 -0400

    Last step before integrating RubyParser.

--- rusche.rb
+++ rusche.rb
@@ -10,8 +10,25 @@ end
 class Rusche
   def reflect_on_module(mod)
     @constants ||= {}
+    @methods ||= {}
+
     mod.constants.each do |constant|
       @constants[constant] = ToCC.const_get(constant)
+    end
+
+    mod.public_instance_methods.each do |method|
+      method_file, method_line = mod.instance_method(method).source_location
+      file_text = File.read(method_file)
+      method_text = []
+      line_number = 0
+      file_text.each_line do |line|
+        # Skip everything before the first line of the actual method definition.
+        line_number += 1
+        next if line_number <= method_line
+        break if line =~ /end/
+        method_text.push line.strip
+      end
+      puts method_text
     end
   end
 
@@ -26,9 +43,6 @@ class Rusche
   def main
     reflect_on_module(ToCC)
     generate_scheme
-    #ToCC.public_instance_methods.each do |method|
-    #  ToCC.send(method)
-    #end
   end
 end
 
